095662eccee1ce33fab2ceb072bba6f0fc71022c
VallejoJuanPablo <juampy.vallejo@gmail.com>
2021-09-20 19:39:35 -0300

    con db

--- src/controllers/excel.js
+++ src/controllers/excel.js
@@ -3,35 +3,52 @@ const db = require("../config/database");
 
 //Controlador de usuarios , contiene todas la funciones realcionadas con el usuario.
 module.exports.test = async (req, res) => {
-var excel = require('exceljs');
-var options = {
-    filename: './src/files/streamed-workbook.xlsx',
-    useStyles: true,
-    useSharedStrings: true
-};
+  const sql = "SELECT * from productos_2 where entidadimportarprecio = '2'";
+  db.query(sql)
+    .then((productos) => {
 
-var workbook = new excel.stream.xlsx.WorkbookWriter(options);
-var worksheet = workbook.addWorksheet('My Sheet');
-worksheet.columns = [
-    { header: 'Id', key: 'id', width: 10 },
-    { header: 'Name', key: 'name', width: 32 },
-    { header: 'D.O.B.', key: 'DOB', width: 10 }
-];
 
-worksheet.addRow({id: 1, name: 'John Doe', dob: new Date(1970,1,1)});
-worksheet.addRow({id: 2, name: 'Jane Doe', dob: new Date(1965,1,7)});
-worksheet.commit();
+      var excel = require("exceljs");
+      var options = {
+        filename: "./src/files/streamed-test.xlsx",
+        useStyles: true,
+        useSharedStrings: true,
+      };
 
-workbook.commit().then(function(){
-    console.log('xls file is written.');
-}); 
-const directoryPath =  "./src/files/streamed-workbook.xlsx";
+      var workbook = new excel.stream.xlsx.WorkbookWriter(options);
+      var worksheet = workbook.addWorksheet("My Sheet");
+      worksheet.columns = [
+        { header: "Id", key: "id", width: 10 },
+        { header: "Name", key: "name", width: 45 },
+        { header: "D.O.B.", key: "DOB", width: 10 },
+      ];
 
-res.download(directoryPath, "streamed-workbook.xlsx", (err) => {
-  if (err) {
-    res.status(500).send({
-      message: "Could not download the file. " + err,
+      productos[0].forEach(element => {
+        worksheet.addRow({ id: element['id'], name: element['producto'], dob: new Date(1970, 1, 1) });
+      });
+      
+    
+      worksheet.commit();
+
+      workbook.commit().then(function () {
+        console.log("xls file is written.");
+      });
+      const directoryPath = "./src/files/streamed-test.xlsx";
+
+      res.download(directoryPath, "streamed-test.xlsx", (err) => {
+        if (err) {
+          res.status(500).send({
+            message: "Could not download the file. " + err,
+          });
+        }
+      });
+    })
+
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).send({
+        status: 500,
+        message: "Error conectando BD.",
+      });
     });
-  }
-});
 };
